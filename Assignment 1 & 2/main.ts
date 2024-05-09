3e944eda2ab448b0df4182726c77bf82a450e518
Shakir Hussain <161478273+shakir-hussain1@users.noreply.github.com>
2024-05-09 12:33:03 +0500

    added console in 1st assignment

--- Assignment 1 & 2/main.ts
+++ Assignment 1 & 2/main.ts
@@ -4,5 +4,7 @@
 
 // Q2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
+console.log("\n" + "WELCOME TO TYPESCRIPT 45 ASSIGNMENTS QUESTIONS TASKS" + "\n");
+
 const personName : string = "Shakir";
 console.log(`"Hello ${personName}, would you like to learn some Typescript today?"`);
