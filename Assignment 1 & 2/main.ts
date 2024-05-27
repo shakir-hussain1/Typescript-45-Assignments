bd9dde950bc39b379f2eb79114615bb19a8d6d34
Shakir Hussain <161478273+shakir-hussain1@users.noreply.github.com>
2024-05-27 17:52:59 +0500

    edit in console

--- Assignment 1 & 2/main.ts
+++ Assignment 1 & 2/main.ts
@@ -1,5 +1,5 @@
 
-console.log("\n" + "WELCOME TO TYPESCRIPT 45 ASSIGNMENTS QUESTIONS TASKS" + "\n");
+console.log("\n" + "WELCOME TO TYPESCRIPT 45 ASSIGNMENTS" + "\n");
 
 // Q1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
 
@@ -7,5 +7,5 @@ console.log("\n" + "WELCOME TO TYPESCRIPT 45 ASSIGNMENTS QUESTIONS TASKS" + "\n"
 
 // Q2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
-const personName : string = "Shakir";
+const personName : string = "Shakir Hussain";
 console.log(`"Hello ${personName}, would you like to learn some Typescript today?"`);
