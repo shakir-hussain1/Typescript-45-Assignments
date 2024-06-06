86fe01d9d9e7402f6b7c7e18775f4724485fa42c
Shakir Hussain <161478273+shakir-hussain1@users.noreply.github.com>
2024-06-07 02:23:21 +0500

    commented

--- Assignment 11/11.js
+++ Assignment 11/11.js
@@ -1,6 +1,6 @@
 // Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-var frndNames = ["Zeeshan", "Akif", "Naeem", "Najeeb", "Zulfiqar"];
-console.log(frndNames[0]);
+var frndNames = ["Zeeshan", "Akif", "Naeem", "Najeeb", "Zulfiqar"]; //array defined
+console.log(frndNames[0]); //printed array values one by one
 console.log(frndNames[1]);
 console.log(frndNames[2]);
 console.log(frndNames[3]);
--- Assignment 11/11.ts
+++ Assignment 11/11.ts
@@ -1,7 +1,8 @@
 // Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 
-let frndNames : string[] = ["Zeeshan", "Akif", "Naeem", "Najeeb", "Zulfiqar"];
-console.log(frndNames[0]);
+let frndNames:string[]=["Zeeshan","Akif","Naeem","Najeeb","Zulfiqar"]; //array defined
+
+console.log(frndNames[0]); //printed array values one by one
 console.log(frndNames[1]);
 console.log(frndNames[2]);
 console.log(frndNames[3]);
